f4c1b83414c5e28cf26dfbe7c60a2dbb98d9ad30
Th3FirstAvenger <teamshadowdc@protonmail.com>
2021-05-02 12:07:14 +0200

    badchars

--- badchars.py
+++ badchars.py
@@ -1,3 +1,6 @@
+from __future__ import print_function
+listRem = "\\x23\\x3c\\x83\\xba".split("\\x")
 for x in range(1, 256):
-  print("\\x"+"{:02x}".format(x),end='')
+    if "{:02x}".format(x) not in listRem:
+        print("\\x" + "{:02x}".format(x), end='')
 
